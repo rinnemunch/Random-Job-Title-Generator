f067fb6260a2cfea31137eb6e4b905e6cd034156
Shaun Fulton <fulton_shaun@yahoo.com>
2025-04-10 06:48:54 -0400

    Add .gitignore to ignore Visual Studio files

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string[] adjectives = { "Senior", "Lead", "Dynamic", "Unpaid", "Future", "Virtual" };
+string[] roles = { "Data Wrangler", "Pixel Architect", "Spreadsheet Ninja", "Cloud Whisperer", "Tech Wizard" }; 
+
+Random rand = new Random(); 
+string adjective = adjectives[rand.Next(adjectives.Length)];
+string role = roles[rand.Next(roles.Length)];
